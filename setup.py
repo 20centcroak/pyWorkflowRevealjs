be009021c013dc8d9912d256ae0c7e09bd6e1045
20centcroak <vpaveau@outook.com>
2020-12-02 08:34:43 +0100

    v0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyWorkflowRevealjs", 
-    version="0.1",
+    version="0.2",
     author="20centCroak",
     author_email="",
     description="revealjs based presentation",
